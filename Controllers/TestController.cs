9a4b3fbb250c44bc4c56605743cc8d33873a79f1
Lindos-Mahadi <lindosmahadi@.com>
2021-10-31 21:07:01 +0600

    AmbiguousMatchException & Set Route Action method

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -7,12 +7,16 @@ using System.Threading.Tasks;
 namespace MyFirst_WebAPI_Project.Controllers
 {
     [ApiController]
-    [Route("test")]
+    [Route("test/[action]")]
     public class TestController : Controller
     {
         public string Index()
         {
             return "hello from index";
+        }
+        public string Index1()
+        {
+            return "hello from index 1";
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,10 @@ namespace MyFirst_WebAPI_Project
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
