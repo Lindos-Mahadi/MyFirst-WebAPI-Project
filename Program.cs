81284484d415a0a3460e1e49c7b19b0100af37ca
Lindos-Mahadi <lindosmahadi@.com>
2021-10-31 19:49:02 +0600

    Initial WebAPI Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirst_WebAPI_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
