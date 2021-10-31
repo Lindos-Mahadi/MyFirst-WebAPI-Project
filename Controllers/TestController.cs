7c6b38938f98e771612db3384caf2650733912cd
Lindos-Mahadi <lindosmahadi@.com>
2021-10-31 21:00:30 +0600

    Add a new test Controller & Create Controllers folder

---
+++ Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirst_WebAPI_Project.Controllers
+{
+    [ApiController]
+    [Route("test")]
+    public class TestController : Controller
+    {
+        public string Index()
+        {
+            return "hello from index";
+        }
+    }
+}
